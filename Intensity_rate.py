f0598261897971e2e182fc5193e4dbcfce7b54c3
Vid <vid.serazin@gmail.com>
2015-12-15 10:46:43 +0100

    New function added

--- Intensity_rate.py
+++ Intensity_rate.py
@@ -16,11 +16,34 @@ def beriInt(poti):
             next(file)
             # print('berem' + str(temp))
             if '"Count Rate"' in temp:
-                print('dela')
+                # print('dela')
                 break
         for line in file:
             temp = line.strip().split('\t')
+            time.append(float(temp[0]))
+            intensity1.append(float(temp[1]))
+            intensity2.append(float(temp[2]))
 
+    return time, intensity1, intensity2
+
+
+def beriTemp(poti):
+    temperatura = []
+    with open(poti, 'r') as file:
+        try:
+            for line in file:
+                temperatura.append(float(line.split(' ')[0]))
+                for i in range(4):
+                    next(file)
+        except StopIteration:
+            pass
+    return temperatura
+
+
+def risi(xdata, ydata1, ydata2, indeks):
+    plt.plot(xdata, ydata1)
+    plt.plot(xdata, ydata2)
+    plt.savefig('/home/vid/IJS/Meritve')  #Test path
 
 
 a = filedialog.askopenfilename(initialdir='/home/vid/IJS/Meritve/1611/')
